a2f8bc38a84cf96a1b930e19061a17f82b5520cb
camziny <cameronziny@gmail.com>
2023-06-04 19:51:53 -0400

    -add python language

--- src/components/LanguagesList.js
+++ src/components/LanguagesList.js
@@ -1,18 +1,25 @@
-import React from "react"
-import javascript from "../assets/javascript.png"
-import typescript from "../assets/typescript.png"
+import React from "react";
+import javascript from "../assets/javascript.png";
+import typescript from "../assets/typescript.png";
+import python from "../assets/python.png";
 
 export const LanguagesList = [
-    {
-        name: "JavaScript",
-        type: "programming-language",
-        image: javascript,
-        link: "https://www.javascript.com/"
-    },
-    {
-        name: "TypeScript",
-        type: "programming-language",
-        image: typescript,
-        link: "https://www.typescriptlang.org/"
-    },
-]
+  {
+    name: "JavaScript",
+    type: "programming-language",
+    image: javascript,
+    link: "https://www.javascript.com/",
+  },
+  {
+    name: "TypeScript",
+    type: "programming-language",
+    image: typescript,
+    link: "https://www.typescriptlang.org/",
+  },
+  {
+    name: "Python",
+    type: "programming-language",
+    image: python,
+    link: "https://www.typescriptlang.org/",
+  },
+];
